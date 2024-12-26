bc3e277c0abdfce5796f9680e74eee31d7bc1bd9
Robert Vokac <robertvokac@robertvokac.com>
2024-12-26 16:24:31 +0100

    Speedy Blupi for Windows Phone is now working in the web browser

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -24,7 +24,7 @@ namespace WindowsPhoneSpeedyBlupi.Pages
             // init game
             if (_game == null)
             {
-                _game = new WindowsPhoneSpeedyBlupiGame();
+                _game = new Game1();
                 _game.Run();
             }
 
